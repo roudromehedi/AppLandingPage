3163059870b8602708f4c17601381335a8a4e7bf
RoudroMehedi <roudro.m@gmail.com>
2023-07-16 19:14:36 +0200

    re-arrange components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <script setup>
-import Navbar from "./components/nav-bar.vue";
-import heroSection from "./components/hero-section.vue";
-import PartnerSection from "./components/partner-section.vue";
-import Operations from "./components/operations-section.vue";
-import About from "./components/about-section.vue";
-import ServiceDeliverySection from "./components/service-delivery-section.vue";
-import TrialSection from "./components/trial-section.vue";
-import Footer from "./components/footer.vue";
+import Homepage from "./pages/home-page.vue";
 </script>
 
 <template>
-  <div class="">
-    <Navbar></Navbar>
-    <heroSection></heroSection>
-    <PartnerSection></PartnerSection>
-    <Operations></Operations>
-    <About></About>
-    <ServiceDeliverySection></ServiceDeliverySection>
-    <TrialSection></TrialSection>
-    <Footer></Footer>
-  </div>
+  <Homepage></Homepage>
 </template>
 
 <style scoped></style>
---
+++ src/pages/home-page.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Navbar from "../components/nav-bar.vue";
+import heroSection from "../components/hero-section.vue";
+import PartnerSection from "../components/partner-section.vue";
+import Operations from "../components/operations-section.vue";
+import About from "../components/about-section.vue";
+import ServiceDeliverySection from "../components/service-delivery-section.vue";
+import TrialSection from "../components/trial-section.vue";
+</script>
+
+<template>
+  <div class="">
+    <Navbar></Navbar>
+    <heroSection></heroSection>
+    <PartnerSection></PartnerSection>
+    <Operations></Operations>
+    <About></About>
+    <ServiceDeliverySection></ServiceDeliverySection>
+    <TrialSection></TrialSection>
+    <Footer></Footer>
+  </div>
+</template>
+
+<style scoped></style>
